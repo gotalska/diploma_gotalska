e6994e4d1e613f7a50d21b7e7d3ed5f85b6829a3
Olga GOtalskaia <gotalska@tut.by>
2022-09-08 15:44:56 +0300

    Добавлено применение Retry в тесты

--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
 import pages.MyAccountPage;
 import pages.RegisterPage;
+import utils.RetryAnalyzer;
 
 
 import static org.testng.Assert.*;
